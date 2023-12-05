9900e8c1922616d08e550ee6e672f85d998a1e40
unknown <sithunaing8209@gmail.com>
2023-12-05 21:16:13 +0630

    error key changed to id

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -6,19 +6,19 @@ import ChecklistIcon from '@mui/icons-material/Checklist';
 import SupportAgentIcon from '@mui/icons-material/SupportAgent';
 
 const features = [
-    {icon : <MenuBookIcon sx={{fontSize : "70px" , color : "primary.main"}} /> ,
+    {id : 1, icon : <MenuBookIcon sx={{fontSize : "70px" , color : "primary.main"}} /> ,
      lable : "Easily manage your menus with Foodie POS",
      delay : "1000ms"},
-    {icon : <QrCode2Icon sx={{fontSize : "70px" , color : "primary.main"}} /> , 
+    {id : 2, icon : <QrCode2Icon sx={{fontSize : "70px" , color : "primary.main"}} /> , 
      lable : "Scan and order.Quick and easy! Your customer will love it!",
      delay : "1200ms"},
-    {icon : <LocationOnIcon sx={{fontSize : "70px" , color : "primary.main"}} /> ,
+    {id : 3, icon : <LocationOnIcon sx={{fontSize : "70px" , color : "primary.main"}} /> ,
      lable : "Foodie POS supports multiple locations for your business.",
      delay : "1400ms"},
-    {icon : <ChecklistIcon sx={{fontSize : "70px" , color : "primary.main"}} /> ,
+    {id : 4, icon : <ChecklistIcon sx={{fontSize : "70px" , color : "primary.main"}} /> ,
      lable : "Backoffice and order apps are included in every subscribtion.",
      delay : "1600ms"},
-    {icon : <SupportAgentIcon sx={{fontSize : "70px" , color : "primary.main"}} /> ,
+    {id : 5, icon : <SupportAgentIcon sx={{fontSize : "70px" , color : "primary.main"}} /> ,
      lable : "Dedicated customer support so that we are always here to help you.",
      delay : "1800ms"},
 ]
@@ -26,7 +26,7 @@ const features = [
 const Features = () => {
     return (
         <Box sx={{ display : "flex" , gap : "100px" , justifyContent : "center" , flexWrap : "wrap" , width : "80vw" , margin : "0 auto" , mt : "30px"}} >
-            {features.map(item => <Zoom key={item.lable} in style={{ transitionDelay : item.delay}}>
+            {features.map(item => <Zoom key={item.id} in style={{ transitionDelay : item.delay}}>
                 <Box sx={{ width : "260px" , textAlign : "center" }}>
                     {item.icon}
                     <Typography>{item.lable}</Typography>
--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -2,15 +2,15 @@ import { Box, Typography } from "@mui/material"
 import Image from "next/image";
 
 const comments = [
-    {imgSrc : "/Tom.jpg" , name : "Tom" , demotion : "Cat Food Co.Lid" , message : "We increased our sale by 120% during the first 3 month of using Foodie POS.Easy and simple to use. Super duper recommended for everyone who are less tech savy. 5/5" },
-    {imgSrc : "/Jerry.jpg" , name : "Jerry" , demotion : "Mouse Food Co.Lid" , message : "Our customers love Foodie POS. Quick and easy with OR code ordering.We now spend more time taking care of our customer instead of taking orders manually. Thanks to Foodie POS!" },
-    {imgSrc : "/Dame.jpg" , name : "Dame" , demotion : "Human Food Co.Lid" , message : "Integrated system . Easy to use . Very satisfied. Highly recommended for everyone , Foodie POS customer service is a top-notch! They are always there when we need help. 5 starsss" },
+    {id: 1 , imgSrc : "/Tom.jpg" , name : "Tom" , demotion : "Cat Food Co.Lid" , message : "We increased our sale by 120% during the first 3 month of using Foodie POS.Easy and simple to use. Super duper recommended for everyone who are less tech savy. 5/5" },
+    {id: 2 ,imgSrc : "/Jerry.jpg" , name : "Jerry" , demotion : "Mouse Food Co.Lid" , message : "Our customers love Foodie POS. Quick and easy with OR code ordering.We now spend more time taking care of our customer instead of taking orders manually. Thanks to Foodie POS!" },
+    {id: 3,imgSrc : "/Dame.jpg" , name : "Dame" , demotion : "Human Food Co.Lid" , message : "Integrated system . Easy to use . Very satisfied. Highly recommended for everyone , Foodie POS customer service is a top-notch! They are always there when we need help. 5 starsss" },
 ]
 
 const Testimonials = () => {
     return (
         <Box sx={{ display : "flex" , justifyContent : "center" , gap : "80px" , mt : "40px"}}>
-            {comments.map(item => <Box key={item.name} sx={{bgcolor : "info.main" , width : "350px" , height : "200px" , borderRadius : "10px"}}>
+            {comments.map(item => <Box key={item.id} sx={{bgcolor : "info.main" , width : "350px" , height : "200px" , borderRadius : "10px"}}>
                 <Box sx={{ display : "flex" , gap : "20px" , alignItems : "center"}}>
                     <Image alt={item.name} src={item.imgSrc} width={55} height={50}  style={{margin : "5px" , borderRadius : "50px"}} />
                     <Box sx={{ fontStyle : "italic"}}>
